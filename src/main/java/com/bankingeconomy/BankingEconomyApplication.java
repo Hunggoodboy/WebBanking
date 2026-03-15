e2f6d47ac67ccb45aa38bdd8bf40ac60811a6d76
Hunggoodboy <Hungmk2005@gmail.com>
2026-03-15 22:05:28 +0700

    add docker for everyone

---
+++ src/main/java/com/bankingeconomy/BankingEconomyApplication.java
@@ -0,0 +1,13 @@
+package com.bankingeconomy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingEconomyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingEconomyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bankingeconomy/BankingEconomyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingeconomy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingEconomyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
