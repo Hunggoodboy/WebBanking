2c111cd56b66580f4c2cb33596b2429112d0686d
trinhvanhao112233 <trinhvanhao112233@gmail.com>
2026-03-28 00:11:05 +0700

    Hào: Hoàn thành tích hợp Hadoop và API

---
+++ WebBanking/src/main/java/com/bankingeconomy/BankingEconomyApplication.java
@@ -0,0 +1,16 @@
+package com.bankingeconomy;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.bankingeconomy.service.HdfsService;
+
+@SpringBootApplication
+public class BankingEconomyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingEconomyApplication.class, args);
+    }
+}
