631a601e35eeb3695d629fc6c7a046640a696d95
HoangKien17 <hoangkien1712005@gmail.com>
2026-04-15 20:30:23 +0700

    15/4

--- src/main/java/com/bankingeconomy/service/consume/TransactionProcessor.java
+++ src/main/java/com/bankingeconomy/service/consume/TransactionProcessor.java
@@ -1,13 +1,15 @@
-package com.bankingeconomy.service.consumer;
+package com.bankingeconomy.service.consume;
 
 
-import com.bankingeconomy.event.TransferEvent;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.UUID;
+
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
+import com.bankingeconomy.event.TransferEvent;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
