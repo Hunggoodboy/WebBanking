ad6e89569b8c457d23d8cf7a2c2793207ceb5f6d
rinnn31 <damquangphongthdk@gmail.com>
2026-04-17 20:18:37 +0700

    change type of output value

--- src/main/java/com/bankingeconomy/hadoop/mapreduce/UserTransactionSumMapper.java
+++ src/main/java/com/bankingeconomy/hadoop/mapreduce/UserTransactionSumMapper.java
@@ -1,5 +1,6 @@
 package com.bankingeconomy.hadoop.mapreduce;
 
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -7,21 +8,23 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class UserTransactionSumMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+public class UserTransactionSumMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
     private final Text outKey = new Text();
-    private final IntWritable outValue = new IntWritable();
+    private final DoubleWritable outValue = new DoubleWritable();
 
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         // Tách dữ liệu đầu vào và kiểm tra có đủ thông tin không
         String record = value.toString();
+        // Bỏ qua csv header
+        if (record.startsWith("user_id")) return;
         String[] recordDatas = record.split(",");
         if (recordDatas.length < 5) return;
 
         String userId = recordDatas[0].trim();
         String date = recordDatas[3].trim();
         String yearMonth = date.substring(0, 7);
-        int amount = Integer.parseInt(recordDatas[4].trim());
+        double amount = Double.parseDouble(recordDatas[4].trim());
 
         // Tạo key theo (userid_tháng)
         outKey.set(userId+"_"+yearMonth);
--- src/main/java/com/bankingeconomy/hadoop/mapreduce/UserTransactionSumReducer.java
+++ src/main/java/com/bankingeconomy/hadoop/mapreduce/UserTransactionSumReducer.java
@@ -1,18 +1,19 @@
 package com.bankingeconomy.hadoop.mapreduce;
 
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
-public class UserTransactionSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+public class UserTransactionSumReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
     @Override
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-        int sum = 0;
+    public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+        double sum = 0;
         for(var value : values) {
             sum += value.get();
         }
-        context.write(key, new IntWritable(sum));
+        context.write(key, new DoubleWritable(sum));
     }
 }
